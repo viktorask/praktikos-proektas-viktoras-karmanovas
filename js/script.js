7c4190e99a86e76ea6074f05b9bc9883465742cf
viktorask <vkarmanovas@gmail.com>
2017-08-10 15:51:49 +0300

    puse darbo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 55.708263, lng: 21.141179};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
